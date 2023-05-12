4a975bf9f4ff2b81f2af9ac8ed14b74bb5b822f2
ebikdeli <ebikdeli@gmail.com>
2023-05-12 16:02:39 +0330

    websocket added

---
+++ scripts/websocket.js
@@ -0,0 +1,26 @@
+const apple = document.querySelector('.apple-price');
+const bitcoin = document.querySelector('.bitcoin-price');
+
+
+const socket = new WebSocket('wss://ws.finnhub.io?token=c26pfh2ad3i8b33nvh7g');
+
+// Connection opened -> Subscribe
+socket.addEventListener('open', function (event) {
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'AAPL'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'BINANCE:BTCUSDT'}))
+    socket.send(JSON.stringify({'type':'subscribe', 'symbol': 'IC MARKETS:1'}))
+});
+
+// Listen for messages
+socket.addEventListener('message', function (event) {
+    // console.log('Message from server ', event.data);
+    rawData = JSON.parse(event.data)
+    console.log(rawData['data']);
+    // console.log(rawData['data'][0]['p']);
+    // apple.innerHTML = rawData['data'][0]['p']
+});
+
+// Unsubscribe
+var unsubscribe = function(symbol) {
+    socket.send(JSON.stringify({'type':'unsubscribe','symbol': symbol}))
+}
